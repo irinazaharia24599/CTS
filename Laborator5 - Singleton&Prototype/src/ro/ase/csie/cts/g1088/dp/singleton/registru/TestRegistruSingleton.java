264cdddbcd8c36807347a29997f81c9f1bff4d1d
Irina Zaharia <irina.zaharia24599@gmail.com>
2021-03-25 13:25:55 +0200

    Lab 5.2

---
+++ Laborator5 - Singleton&Prototype/src/ro/ase/csie/cts/g1088/dp/singleton/registru/TestRegistruSingleton.java
@@ -0,0 +1,16 @@
+package ro.ase.csie.cts.g1088.dp.singleton.registru;
+
+public class TestRegistruSingleton {
+	public static void main(String[] args) {
+		ConexiuneBD conexiune1 = ConexiuneBD.getConexiune("10.0.0.1", "cts");
+		ConexiuneBD conexiune2 = ConexiuneBD.getConexiune("10.0.0.2", "cts");
+		
+		if(conexiune1 == conexiune2) {
+			System.out.println("Cele 2 referinte sunt identice");
+		}
+		else {
+			System.out.println("Cele 2 referinte sunt diferite");
+		}
+
+	}
+}
