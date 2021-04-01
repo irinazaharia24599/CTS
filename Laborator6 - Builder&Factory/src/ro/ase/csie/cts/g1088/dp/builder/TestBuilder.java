3f645e2016d1fa327e60d8f98c629714562caa15
Irina Zaharia <irina.zaharia24599@gmail.com>
2021-04-01 12:41:09 +0300

    lab 6.1

---
+++ Laborator6 - Builder&Factory/src/ro/ase/csie/cts/g1088/dp/builder/TestBuilder.java
@@ -0,0 +1,19 @@
+package ro.ase.csie.cts.g1088.dp.builder;
+
+public class TestBuilder {
+
+	public static void main(String[] args) {
+
+//		SuperErou superErou = new SuperErou();
+//		
+//		SuperErou superErou2 = new SuperErou("Superman", 100, false, false, new Arma(), null, new Laser(), null);
+		
+		SuperErou superman = new SuperErou.SuperErouBuilder("Superman", 100).build();
+		SuperErou joker = new SuperErou.SuperErouBuilder("Joker", 200)
+				.esteNegativ()
+				.esteRanit()
+				.setArmaDreapta(new Arma())
+				.build();
+	}
+
+}
