a115ffee4c2ed06377148afdd41af15b28dbd35e
Irina Zaharia <irina.zaharia24599@gmail.com>
2021-04-08 12:35:45 +0300

    lab 7.1

---
+++ Laborator 7 - Factory Method&Facade/src/ro/ase/csie/cts/g1088/dp/factory/method/FactoryAbstractCaracter.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.g1088.dp.factory.method;
+
+public abstract class FactoryAbstractCaracter {
+
+	//nivel abstract din Factory Method
+	public abstract CaracterJoc getCaracter(TipCaracter tip, String nume);
+}
