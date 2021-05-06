0950b845669c7d9e0d6e6de32b1ee7c6eeaffd92
Irina Zaharia <irina.zaharia24599@gmail.com>
2021-05-06 13:31:42 +0300

    lab 10.2 - State&Chain

---
+++ Laborator 10 - Strategy&State/src/ro/ase/csie/cts/g1088/dp/state/InterfataModDeplasare.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.g1088.dp.state;
+
+public interface InterfataModDeplasare {
+
+	//public void seMisca();
+
+	public void seMisca(SuperErou erou);
+}
---
+++ Laborator 10 - Strategy&State/src/ro/ase/csie/cts/g1088/dp/state/StareDeplasareRanitCritic.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.g1088.dp.state;
+
+public class StareDeplasareRanitCritic implements InterfataModDeplasare {
+
+	@Override
+	public void seMisca(SuperErou erou) {
+		System.out.println(erou.nume + " nu se deplaseaza, e ranit critic");
+		
+	}
+
+}
