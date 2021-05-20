c195840d96d48d585d17f73181c7249a96ed822a
Irina Zaharia <irina.zaharia24599@gmail.com>
2021-05-20 13:32:15 +0300

    lab 12 - unit testing

---
+++ Laborator 12 - Testare Unitara/src/ro/ase/csie/cts/g1088/testare/main/Aplicatie.java
@@ -0,0 +1,23 @@
+package ro.ase.csie.cts.g1088.testare.main;
+
+import ro.ase.csie.cts.g1088.testare.modele.OperatiiMatematice;
+
+public class Aplicatie {
+
+	public static void main(String[] args) {
+
+		System.out.println("Aplicatia ruleaza");
+		
+//		int a = 5;
+//		int b = 6;
+//		int sumaAsteptata = 11;
+//		int sumaCalculata = OperatiiMatematice.suma(a, b);
+//		if (sumaAsteptata == sumaCalculata) {
+//			System.out.println("suma este corecta");
+//		}
+//		else {
+//			System.out.println("ne trebuie un curs de mate");
+//		}
+	}
+
+}
